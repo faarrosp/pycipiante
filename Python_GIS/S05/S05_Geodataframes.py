df7c666d3c3bb3561e44ba42c5fbde77acfbbb66
felipearrospide <60197764+faarrosp@users.noreply.github.com>
2022-04-18 23:11:20 -0400

    update S05_Geodataframes

--- Python_GIS/S05/S05_Geodataframes.py
+++ Python_GIS/S05/S05_Geodataframes.py
@@ -18,3 +18,54 @@ path = os.path.join('..', 'shapes', 'PROVINCIA', 'PROVINCIAS_2020.shp')
 # 2do paso: asignar una variable el geodataframe
 
 geodataframe = gpd.read_file(path)
+
+# 3er paso: explorar el geodataframe con algunos comandos
+
+# imprimir el sistema de coordenadas
+print(geodataframe.crs)
+
+# explorar las primeras 5 filas del geodataframe
+print(geodataframe.head())
+
+# imprimir todas las columnas
+print(geodataframe.columns)
+
+# imprimir las filas
+print(geodataframe.index)
+
+for i in geodataframe.index:
+    print(i)
+    
+indices = geodataframe.index
+columnas = geodataframe.columns
+
+# imprimir el tamaño de nuestro geodataframe
+print(geodataframe.shape)
+
+columnas = list(geodataframe.columns)
+
+# imprimir los nombres de un campo en particular
+
+# a) usando la notacion .
+print(geodataframe.REGION)
+
+regiones = list(geodataframe.REGION)
+
+# b) usando la notacion de parentesis cuadrados []
+
+print(geodataframe['REGION'])
+
+regiones = list(geodataframe['REGION'])
+
+# 4to filtrar un dataframe
+
+filtro = geodataframe['REGION'] == 'Coquimbo'
+
+geodataframe_filtrado = geodataframe[filtro].copy()
+
+# 5to guardar el geodataframe filtrado
+
+geodataframe_filtrado.to_file('provincias_Coquimbo.shp')
+
+
+
